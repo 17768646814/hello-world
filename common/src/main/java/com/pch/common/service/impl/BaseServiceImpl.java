2d24d09c3d805697b206f2f2ae8fae367c394674
17768646814 <651158394@qq.com>
2017-01-09 22:50:07 +0800

    first project

---
+++ common/src/main/java/com/pch/common/service/impl/BaseServiceImpl.java
@@ -0,0 +1,15 @@
+package com.pch.common.service.impl;
+
+import com.pch.common.dao.BaseDao;
+import com.pch.common.service.BaseService;
+
+import java.io.Serializable;
+
+/**
+ * @author uo712
+ * @version 1.0
+ * @since 2017/1/9
+ */
+public abstract class BaseServiceImpl<M extends Serializable, PK extends Serializable> implements BaseService<M, PK> {
+    abstract protected BaseDao<M, PK> getBaseDao();
+}
---
+++ study/src/main/java/com/pch/study/App.java
@@ -0,0 +1,18 @@
+package com.pch.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+/**
+ * @author uo712
+ * @version 1.0
+ * @since 2017/1/9
+ */
+@SpringBootApplication
+public class App extends SpringBootServletInitializer {
+    public static void main(String[] args) {
+        SpringApplication springApplication = new SpringApplication(App.class);
+        springApplication.run(args);
+    }
+}
