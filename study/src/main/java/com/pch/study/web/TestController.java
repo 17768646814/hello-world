2e31dd2e6c0e66f9636a096c59ae5feb21b95853
17768646814 <651158394@qq.com>
2017-01-11 18:10:58 +0800

    add pages
    add pages

--- study/src/main/java/com/pch/study/web/TestController.java
+++ study/src/main/java/com/pch/study/web/TestController.java
@@ -1,5 +1,6 @@
 package com.pch.study.web;
 
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/")
 public class TestController {
 
+    @ApiOperation(value = "默认页面", tags = "home")
     @RequestMapping(value = "", method = {RequestMethod.POST, RequestMethod.GET})
     public String hello() {
         return "hello world";
