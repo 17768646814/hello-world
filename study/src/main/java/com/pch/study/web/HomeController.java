2788bc73cf36bfffcdc0b0a4ac0a3197a91b2c13
17768646814 <651158394@qq.com>
2017-01-12 00:19:42 +0800

    add some pages

--- study/src/main/java/com/pch/study/web/HomeController.java
+++ study/src/main/java/com/pch/study/web/HomeController.java
@@ -1,5 +1,8 @@
 package com.pch.study.web;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,6 +17,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
  */
 @RestController
 @RequestMapping("/")
+@Api(value = "测试类",tags = "测试接口")
 public class HomeController {
 
     @RequestMapping(value = "", method = {RequestMethod.POST, RequestMethod.GET})
@@ -27,5 +31,11 @@ public class HomeController {
             modelAndView.addObject("jsonParams", jsonParams);
         }
         return modelAndView;
+    }
+
+    @RequestMapping(value = "/say", method = {RequestMethod.POST, RequestMethod.GET})
+    @ApiOperation(value = "默认页面", notes = "home")
+    public String say(@ApiParam(name = "word",value = "语句",required = false) @RequestParam(required = false, defaultValue = "nothing") String word) {
+        return String.format("you said => %s", word);
     }
 }
